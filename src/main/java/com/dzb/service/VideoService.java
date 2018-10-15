e606b5b0b4700449a05b20bfe74b369ef08851b2
奥利奥 <hfm003@163.com>
2018-10-15 09:47:13 +0800

    Upload VideoService.java

---
+++ src/main/java/com/dzb/service/VideoService.java
@@ -0,0 +1,38 @@
+package com.dzb.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//import com.dzb.model.File;
+/**
+ * @author: HeAria
+ */
+
+public void setVideoService(VideoService videoService){
+        this.videoService = videoService;
+        }
+
+public interface VideoService {
+
+    //文件上传
+    //2018.9.30
+    //
+    @Controller
+    @RequestMapping(value = "/api/file/upload",method = RequestMethod.POST)
+    public @ResponseBody
+    String upload() {
+        String string = upload();
+        return string;
+    }
+
+
+    /*删除文件
+ 2018.10.1
+ */
+    @RequestMapping(value = "/api/file/delete" , method = RequestMethod.DELETE)
+    public void deleteFile() {
+        deleteFile(, );
+    }
+}
