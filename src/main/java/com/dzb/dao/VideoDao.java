23c1417b54af17a5d1b79a7e7d9bbf69bda990b1
奥利奥 <hfm003@163.com>
2018-10-25 01:12:18 +0800

    VideoDao

--- src/main/java/com/dzb/dao/VideoDao.java
+++ src/main/java/com/dzb/dao/VideoDao.java
@@ -1,41 +1,20 @@
-/*
 package com.dzb.dao;
 
+
+import com.dzb.commons.Result;
 import com.dzb.model.Video;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Map;
-
-*/
-/**
- * @description: 描述
- * @author: HeAria
- * @date: 2018-10-08
- *//*
-
 
 public interface VideoDao {
-    */
-/**
+    /**
      * 返回视频列表
-     *  @return
-     *//*
-
-    List<Video> queryNewsList();
-
-    */
-/**
-     * 创建新闻
      * @return
-     *//*
+     */
+    List<Video> queryVideoList();
 
-    int addVideo(Video video);
-
-    */
-/**
-     * 删除指定新闻
-     * @return
-     *//*
+    int uploadVideo(Video video);
 
     int deleteVideo(int video);
-}*/
+}
