c0490e1eb77167d5d5de406c085508a967f93f99
ZzzzzT <zzzzz_t@163.com>
2018-10-25 02:19:09 +0800

    权限判定role修改和保存接口检验修改

---
+++ src/main/java/com/dzb/controller/UserManageController.java
@@ -0,0 +1,95 @@
+package com.dzb.controller;
+
+import com.dzb.commons.Result;
+import com.dzb.model.User;
+import com.dzb.model.UserListInformation;
+import com.dzb.service.UserManageService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.annotation.Timed;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: pinnuli
+ * @date: 18-9-26
+ */
+@RestController
+@RequestMapping("/api/usermanage")
+public class UserManageController {
+    @Autowired
+    private UserManageService userManageService;
+
+    @RequestMapping(value = "/userlist", method = RequestMethod.GET)
+    public Result getUserList(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /**
+         * 获取用户简略信息表
+        */
+        HttpSession session = request.getSession();
+        System.out.println("currentUser" + session.getAttribute("currentUser"));
+        User currentUser;
+        currentUser=(User)session.getAttribute("currentUser");
+        if(!currentUser.getRole().equals("支委")){
+            return Result.createBySuccessMessage("没有权限访问！");
+        }
+        List<User> userList = userManageService.selectAllUser();
+        List<UserListInformation> userListInformations = new ArrayList<>();
+        for (User user : userList) {
+            userListInformations.add(new UserListInformation(user.getName(), user.getStudentNum(), user.getJoinPartyTime(), user.getIdentity()));
+        }
+        Map<String, Object> data = new HashMap<>();
+        data.put("userlist", userListInformations);
+        return Result.createBySuccess(data);
+    }
+
+    @RequestMapping(value = "/detailinfo/{studentNum}", method = RequestMethod.GET)
+    public Result getUserInformation( long studentNum, HttpServletRequest request) throws IOException {
+        /**
+         * 根据学号获取用户个人信息
+         */
+        HttpSession session = request.getSession();
+        System.out.println("currentUser" + session.getAttribute("currentUser"));
+        User currentUser;
+        currentUser=(User)session.getAttribute("currentUser");
+        if(!currentUser.getRole().equals("支委")){
+            return Result.createBySuccessMessage("没有权限访问！");
+        }
+        User user = this.userManageService.getUserInformation(studentNum);
+        Map<String, Object> data = new HashMap<>();
+        data.put("user", user);
+        return Result.createBySuccess(data);
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public Result<String> deleteUser(long studentNum,HttpServletRequest request)  {
+        /**
+         * 根据学号删除用户个人信息
+         */
+        HttpSession session = request.getSession();
+        System.out.println
+
+                ("currentUser" + session.getAttribute("currentUser"));
+        User currentUser;
+        currentUser=(User)session.getAttribute("currentUser");
+        if(!currentUser.getRole().equals("支委")){
+            return Result.createBySuccessMessage("没有权限访问！");
+        }
+        int flat = userManageService.delete(studentNum);
+        if (flat == 0) {
+            return Result.createBySuccessMessage("删除失败");
+        }
+        return Result.createBySuccessMessage("删除成功");
+    }
+
+
+}
